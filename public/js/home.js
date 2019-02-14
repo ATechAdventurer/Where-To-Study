fd88241667a14942f52cd322ae5d7f52988d83e1
Cameron Steele <Cam.steeleis@gmail.com>
2019-02-13 20:48:39 -0600

    Mapbox Styling

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFub3N0ZWVsZSIsImEiOiJjamhtanE1OWswZWFmM2RyeDd3M3pleDdnIn0.Ccam63uwLV2GkhuDt-8DlQ'; // replace this with your access token
     var map = new mapboxgl.Map({
       container: 'map',
-      style: 'your-style-URL-here', // replace this with your style URL
-      center: [-87.661557, 41.893748],
+      style: 'mapbox://styles/mapbox/streets-v9', // replace this with your style URL
+      center: [-96.752323, 32.9854185],
       zoom: 10.7
     });

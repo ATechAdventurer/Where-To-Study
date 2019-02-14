427e30f95d576a43288e19838d2d3d8a2d0b0c75
Cameron Steele <Cam.steeleis@gmail.com>
2019-02-13 20:38:43 -0600

    Fixed Stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 /* eslint-env jquery, browser */
 $(document).ready(() => {
-
-  // Place JavaScript code here...
+  
+  if(window.location.href.indexOf('?ref=producthunt'){
+    alert("You came from product hunt: Hi");
+  }
+  
 
 });

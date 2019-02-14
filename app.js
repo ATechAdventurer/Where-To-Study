49ad2a9de06ba8b6bc05ca94f96d5840e00b5ff9
Cameron Steele <cam.steeleis@gmail.com>
2019-02-14 00:58:48 -0600

    Connected Place Controller to app.js Routes

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
-
+const placeController = require('./controllers/place');
 /**
  * API keys and Passport configuration.
  */
@@ -142,7 +142,8 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
+app.get('/places', placeController.getPlaces);
+app.post('/places/create', placeController.addnewPlaces);
 /**
  * API examples routes.
  */

aaa094dd84d54ec4469d081b3f091324a609584c
Cameron Steele <cam.steeleis@gmail.com>
2019-02-14 00:54:28 -0600

    Created places controller

---
+++ controllers/place.js
@@ -0,0 +1,56 @@
+const { promisify } = require('util');
+const Place = require('../models/Place');
+const User = require('../models/User');
+const toTitleCase = require('../utils/toTitleCase');
+
+const randomBytesAsync = promisify(crypto.randomBytes);
+
+/**
+ * GET /places
+ * Places page.
+ */
+exports.getPlaces = (req, res) => {
+    res.render('places/index', {
+        title: 'Places'
+    });
+};
+
+/** 
+ * GET /places/new
+ * Add Place page
+ */
+
+ exports.newPlaces = (req, res) => {
+     res.render('places/create', {
+         title: "New Place"
+     })
+ }
+
+/**
+ * POST /places
+ * Create a new local account.
+ */
+exports.addnewPlaces = (req, res, next) => {
+    //TODO: Add validation
+    User.findOne({ email: req.body.email }, (err, foundUser) => {
+        if (err) { return next(err); }
+        if (!foundUser) { return next("Please login") }
+        if (foundUser) {
+            let { title, description, rating, location, address, website, tags } = req.body;
+            const place = new Place({
+                title,
+                description,
+                rating,
+                location,
+                address,
+                website,
+                tags,
+                submitedBy: foundUser._id
+            });
+            Place.save((err) => {
+                if (err) { return next(err); }
+                res.redirect('/places');
+            })
+        }
+    };
+}

f5702dda30f3d9c6df1c1553115bc3e096aae009
Cameron Steele <Cam.steeleis@gmail.com>
2019-02-17 19:11:19 -0600

    Added Styling of map

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,7 @@ if (window.location.href == "https://www.wheretostudy.app/") {
   mapboxgl.accessToken = 'pk.eyJ1IjoibWFub3N0ZWVsZSIsImEiOiJjamhtanE1OWswZWFmM2RyeDd3M3pleDdnIn0.Ccam63uwLV2GkhuDt-8DlQ'; // replace this with your access token
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9', // replace this with your style URL
+    style: 'mapbox://styles/manosteele/cjs9n81wu0gi81foeh4gh59mm', // replace this with your style URL
     center: [-96.752323, 32.9854185],
     zoom: 10.7
   });
